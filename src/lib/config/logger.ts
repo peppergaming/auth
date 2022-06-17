1fcecdb23cabe0420575a7a7f24b63e586112afa
Felipe Cardoso <felipe.cardoso@supsi.ch>
2022-06-17 20:25:16 +0200

    Add config with logger

---
+++ src/lib/config/logger.ts
@@ -0,0 +1,30 @@
+import log from 'loglevel';
+import logPrefix from 'loglevel-plugin-prefix'
+
+export type LogLevel = log.LogLevelDesc
+
+export const DEFAULT_LEVEL: LogLevel = "error"
+export const LOGGER_PREFIX = "pepper"
+
+
+logPrefix.apply(log, {
+  format(level, name, timestamp) {
+    return `[${timestamp}] ${level} ${name}:`
+  },
+});
+
+
+const logger = log.getLogger("pepper")
+
+logger.setLevel(DEFAULT_LEVEL);
+
+export const setLoggerLevel = (level: LogLevel) => {
+  logger.setLevel(level);
+}
+
+export const getLogger = (name: string, level: LogLevel = DEFAULT_LEVEL) => {
+  const customLogger = log.getLogger(`${LOGGER_PREFIX} ${name}`)
+  customLogger.setLevel(level)
+}
+
+export default logger;
