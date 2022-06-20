0a4f2c816221da592a84ca25c81dbcc5d4018e84
Felipe Cardoso <felipe.cardoso@supsi.ch>
2022-06-20 19:34:32 +0200

    Refactor PepperWallet as a Wrapper of ethers wallet
    #3

--- src/lib/wallet/index.ts
+++ src/lib/wallet/index.ts
@@ -1,9 +1,14 @@
+import { TransactionRequest } from '@ethersproject/abstract-provider';
+import { Bytes } from '@ethersproject/bytes';
+import { ProgressCallback } from '@ethersproject/json-wallets';
 import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
 import { Wallet } from 'ethers';
 
 import logger from '../config/logger';
 
-export class PepperWallet extends Wallet {
+export class PepperWallet {
+  readonly #wallet: Wallet;
+
   constructor(adapter: OpenloginAdapter) {
     if (
       !adapter ||
@@ -13,6 +18,35 @@ export class PepperWallet extends Wallet {
       logger.debug('Pepper wallet received an invalid adapter: ', adapter);
       throw new Error('Unable to instantiate Pepper Wallet');
     }
-    super(`0x${adapter.openloginInstance.privKey}`);
+
+    this.#wallet = new Wallet(`0x${adapter.openloginInstance.privKey}`);
+  }
+
+  get address() {
+    return this.#wallet.address;
+  }
+
+  get publicKey() {
+    return this.#wallet.publicKey;
+  }
+
+  get provider() {
+    return this.#wallet.provider;
+  }
+
+  async signTransaction(transaction: TransactionRequest): Promise<string> {
+    return await this.#wallet.signTransaction(transaction);
+  }
+
+  async signMessage(message: Bytes | string): Promise<string> {
+    return await this.#wallet.signMessage(message);
+  }
+
+  async encrypt(
+    password: Bytes | string,
+    options?: any,
+    progressCallback?: ProgressCallback
+  ): Promise<string> {
+    return await this.#wallet.encrypt(password, options, progressCallback);
   }
 }
