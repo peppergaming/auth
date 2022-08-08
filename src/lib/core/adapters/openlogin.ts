13cdcc13dc40c20210f517db9c7f64b84c210c87
Felipe Cardoso <felipe.cardoso@hotmail.it>
2022-08-08 16:02:42 +0200

    Fix rpc target in openlogin chain config

--- src/lib/core/adapters/openlogin.ts
+++ src/lib/core/adapters/openlogin.ts
@@ -4,9 +4,10 @@ import { ChainConfig } from '../../../types';
 import {
   CHAIN_NAMESPACES,
   isDev,
+  PEPPER_INFURA_ID,
   WEB3AUTH_CLIENT_ID,
 } from '../../config/constants';
-
+import logger from '../../config/logger';
 export const UX_MODE = {
   POPUP: 'popup',
   REDIRECT: 'redirect',
@@ -18,15 +19,24 @@ export const openLoginAdapterBuilder = async (
   chainConfig?: ChainConfig
 ): Promise<OpenloginAdapter> => {
   const network = isDev ? 'testnet' : 'mainnet';
+  const infuraNetwork = isDev ? 'rinkeby' : 'mainnet';
 
+  const currentChainConfig = {
+    chainId: chainConfig?.chainId || '1',
+    chainNamespace: chainConfig.chainNamespace || CHAIN_NAMESPACES.EIP155,
+    rpcTarget:
+      chainConfig?.rpcTarget ||
+      `https://${infuraNetwork}.infura.io/v3/${PEPPER_INFURA_ID}`,
+    displayName: chainConfig?.name || 'default',
+  };
+
+  logger.debug(
+    'Initializing OpenLogin Adapter with chain config: ',
+    chainConfig
+  );
   return new OpenloginAdapter({
-    chainConfig: {
-      chainId: chainConfig?.chainId || '1',
-      // @ts-ignore
-      chainNamespace: chainConfig.chainNamespace || CHAIN_NAMESPACES.EIP155,
-      rpcTarget: chainConfig?.rpcTarget || '',
-      displayName: chainConfig?.name || 'default',
-    },
+    // @ts-ignore
+    chainConfig: currentChainConfig,
     adapterSettings: {
       network: network,
       clientId: clientID,
