f04419228d93bee36c59ef5d3cfa607ec578f370
Felipe Cardoso <felipe.cardoso@hotmail.it>
2022-08-17 14:30:42 +0200

    Set PepperWallet as ether.js Signer compatible

--- src/lib/wallet/index.ts
+++ src/lib/wallet/index.ts
@@ -1,5 +1,6 @@
-import { TransactionRequest } from '@ethersproject/abstract-provider';
+import { Provider, TransactionRequest } from '@ethersproject/abstract-provider';
 import { Bytes } from '@ethersproject/bytes';
+import { defineReadOnly } from '@ethersproject/properties';
 import { JsonRpcProvider, JsonRpcSigner } from '@ethersproject/providers';
 import { OpenloginAdapter } from '@web3auth/openlogin-adapter';
 import { Signer, Wallet } from 'ethers';
@@ -7,15 +8,25 @@ import { Signer, Wallet } from 'ethers';
 import { ChainConfig } from '../../types';
 import logger from '../config/logger';
 
-export abstract class PepperWallet {
+export class PepperWallet extends Signer {
   readonly #wallet: Signer;
   private _address: string;
   private _publicKey: string;
 
   protected constructor(wallet: Signer, address = '', publicKey = '') {
+    super();
     this.#wallet = wallet;
     this._address = address;
     this._publicKey = publicKey;
+    defineReadOnly(this, 'provider', this.#wallet.provider || null);
+  }
+
+  getAddress(): Promise<string> {
+    return Promise.resolve(this.address);
+  }
+
+  connect(provider: Provider): Signer {
+    return this.#wallet.connect(provider);
   }
 
   get address() {
@@ -34,9 +45,9 @@ export abstract class PepperWallet {
     this._publicKey = publicKey;
   }
 
-  get provider() {
-    return this.#wallet.provider;
-  }
+  // get provider() {
+  //   return this.#wallet.provider;
+  // }
 
   async signTransaction(transaction: TransactionRequest): Promise<string> {
     return await this.#wallet.signTransaction(transaction);
